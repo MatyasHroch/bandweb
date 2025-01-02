dac1d25fd3637ac04e1e6bd755dc953547978ad6
MatyasHroch <matyas.hroch01@upol.cz>
2025-01-02 16:47:32 +0100

    icons repaired

--- band-web/app/records/page.tsx
+++ band-web/app/records/page.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import RecordList from '../../components/RecordList';
 import { Record } from '../../data/recordsData';
 import { IconLink } from '../../components/icons/IconLink';
+import { faSpotify } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 
 const Records: React.FC = () => {
     return (
@@ -11,14 +14,14 @@ const Records: React.FC = () => {
             </h1>
             <h2 className="text-foreground text-3xl font-bold my-8 text-start flex items-center">
                 <IconLink href="https://open.spotify.com/album/4DDxT2CDitBvQF3Xiz49eF?si=u16hBpd3QF6irPspgL8HMQ" >
-                    <svg className="fill-foreground pl-0 p-4 " role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Spotify</title><path d="M12 0C5.4 0 0 5.4 0 12s5.4 12 12 12 12-5.4 12-12S18.66 0 12 0zm5.521 17.34c-.24.359-.66.48-1.021.24-2.82-1.74-6.36-2.101-10.561-1.141-.418.122-.779-.179-.899-.539-.12-.421.18-.78.54-.9 4.56-1.021 8.52-.6 11.64 1.32.42.18.479.659.301 1.02zm1.44-3.3c-.301.42-.841.6-1.262.3-3.239-1.98-8.159-2.58-11.939-1.38-.479.12-1.02-.12-1.14-.6-.12-.48.12-1.021.6-1.141C9.6 9.9 15 10.561 18.72 12.84c.361.181.54.78.241 1.2zm.12-3.36C15.24 8.4 8.82 8.16 5.16 9.301c-.6.179-1.2-.181-1.38-.721-.18-.601.18-1.2.72-1.381 4.26-1.26 11.28-1.02 15.721 1.621.539.3.719 1.02.419 1.56-.299.421-1.02.599-1.559.3z" /></svg>
+                    <FontAwesomeIcon className="fill-foreground p-0 h-12 w-12" icon={faSpotify} />
                 </IconLink>
                 Lesanten (2024)
             </h2>
             <RecordList filter={(record: Record) => record.album == "Lesanten"} />
             <h2 className="text-foreground text-3xl font-bold my-8 text-start flex items-center">
                 <IconLink href="https://open.spotify.com/album/3vENEMs8lrfouP3COsjqAg?si=OjbP858NRf25icX-AEaWaw" >
-                    <svg className="fill-foreground  pl-0 p-4" role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><title>Spotify</title><path d="M12 0C5.4 0 0 5.4 0 12s5.4 12 12 12 12-5.4 12-12S18.66 0 12 0zm5.521 17.34c-.24.359-.66.48-1.021.24-2.82-1.74-6.36-2.101-10.561-1.141-.418.122-.779-.179-.899-.539-.12-.421.18-.78.54-.9 4.56-1.021 8.52-.6 11.64 1.32.42.18.479.659.301 1.02zm1.44-3.3c-.301.42-.841.6-1.262.3-3.239-1.98-8.159-2.58-11.939-1.38-.479.12-1.02-.12-1.14-.6-.12-.48.12-1.021.6-1.141C9.6 9.9 15 10.561 18.72 12.84c.361.181.54.78.241 1.2zm.12-3.36C15.24 8.4 8.82 8.16 5.16 9.301c-.6.179-1.2-.181-1.38-.721-.18-.601.18-1.2.72-1.381 4.26-1.26 11.28-1.02 15.721 1.621.539.3.719 1.02.419 1.56-.299.421-1.02.599-1.559.3z" /></svg>
+                    <FontAwesomeIcon className="fill-foreground p-0 h-12 w-12" icon={faSpotify} />
                 </IconLink>
                 Na Jabloni (2003)
             </h2>
--- band-web/components/Socials.tsx
+++ band-web/components/Socials.tsx
@@ -3,23 +3,22 @@ import { IconLink } from './icons/IconLink';
 import { faFacebook, faSpotify, faSoundcloud, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-
 const Socials: React.FC = () => {
     return (
         <div className="bg-background">
             <h2 className="text-2xl font-bold text-accent mb-6">Najít nás můžete i zde</h2>
             <div className="flex md:gap-4 gap-2">
                 <IconLink href="https://www.youtube.com/watch?v=tFD8BZKMEj0&list=OLAK5uy_l1AoVrZA-dv2VE2OezuAbnSvWit2-NHjM">
-                    <FontAwesomeIcon className="fill-foreground p-4 border border-accent rounded-2xl h-16 w-16" icon={faYoutube} />
+                    <FontAwesomeIcon className="fill-foreground p-4 border border-accent rounded-2xl h-8 w-8" icon={faYoutube} />
                 </IconLink>
                 <IconLink href="https://soundcloud.com/lesanten" >
-                    <FontAwesomeIcon className="fill-foreground p-4 border border-accent rounded-2xl h-16 w-16" icon={faSoundcloud} />
+                    <FontAwesomeIcon className="fill-foreground p-4 border border-accent rounded-2xl h-8 w-8" icon={faSoundcloud} />
                 </IconLink>
                 <IconLink href="https://open.spotify.com/album/4DDxT2CDitBvQF3Xiz49eF?si=u16hBpd3QF6irPspgL8HMQ" >
-                    <FontAwesomeIcon className="fill-foreground p-4 border border-accent rounded-2xl h-16 w-16" icon={faSpotify} />
+                    <FontAwesomeIcon className="fill-foreground p-4 border border-accent rounded-2xl h-8 w-8" icon={faSpotify} />
                 </IconLink>
                 <IconLink href="https://facebook.com/lesanten" >
-                    <FontAwesomeIcon className="fill-foreground p-4 border border-accent rounded-2xl h-16 w-16" icon={faFacebook} />
+                    <FontAwesomeIcon className="fill-foreground p-4 border border-accent rounded-2xl h-8 w-8" icon={faFacebook} />
                 </IconLink>
             </div>
         </div>
